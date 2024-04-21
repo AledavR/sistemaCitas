fbf4167084e9d340d560cd79f226a28b3439972c
AledavR <150876244+AledavR@users.noreply.github.com>
2024-04-20 20:45:48 -0500

    Removidos importes innecesarios
    Se removieron importes los archivos ControladorPaciente y ControladorRegistro.

--- src/main/java/unmsm/hospital/sistemaCitas/controllers/ControladorPaciente.java
+++ src/main/java/unmsm/hospital/sistemaCitas/controllers/ControladorPaciente.java
@@ -3,20 +3,15 @@ package unmsm.hospital.sistemaCitas.controllers;
 import unmsm.hospital.sistemaCitas.models.Paciente;
 import unmsm.hospital.sistemaCitas.services.ServicioPaciente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.template.PathBasedTemplateAvailabilityProvider;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
-
 
 import java.util.List;
 
